21595833beb786f0b64b5ec777345062ca7a9a60
Vaibhav Mishra <vinu76jsr@gmail.com>
2020-06-22 16:10:58 +0530

    Chapter 11 code added

--- src/allocation/service_layer/handlers.py
+++ src/allocation/service_layer/handlers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from allocation.adapters import email
+from allocation.adapters import email, redis_eventpublisher
 from allocation.domain import commands, events, model
 from allocation.domain.model import OrderLine
 
@@ -48,3 +48,9 @@ def send_out_of_stock_notification(
     email.send(
         "stock@made.com", f"Out of stock for {event.sku}",
     )
+
+
+def publish_allocated_event(
+    event: events.Allocated, uow: unit_of_work.AbstractUnitOfWork,
+):
+    redis_eventpublisher.publish("line_allocated", event)
