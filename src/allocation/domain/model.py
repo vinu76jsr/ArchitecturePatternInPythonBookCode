66f066d93d2255b9c598201aba78a6034baf8afd
Vaibhav Mishra <vinu76jsr@gmail.com>
2020-06-22 10:27:40 +0530

    Chapter 9 code

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional, Set, List
+from typing import Optional, List, Set
 from . import events
 
 
@@ -20,6 +20,15 @@ class Product:
             return batch.reference
         except StopIteration:
             self.events.append(events.OutOfStock(line.sku))
+
+    def change_batch_quantity(self, ref: str, qty: int):
+        batch = next(b for b in self.batches if b.reference == ref)
+        batch._purchased_quantity = qty
+        while batch.available_quantity < 0:
+            line = batch.deallocate_one()
+            self.events.append(
+                events.AllocationRequired(line.orderid, line.sku, line.qty)
+            )
 
 
 @dataclass(unsafe_hash=True)
@@ -59,9 +68,8 @@ class Batch:
         if self.can_allocate(line):
             self._allocations.add(line)
 
-    def deallocate(self, line: OrderLine):
-        if line in self._allocations:
-            self._allocations.remove(line)
+    def deallocate_one(self) -> OrderLine:
+        return self._allocations.pop()
 
     @property
     def allocated_quantity(self) -> int:
